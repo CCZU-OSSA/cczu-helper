d945bcf7fb5c70ff617ae1bef3fdcddcafdfc3e2
H2Sxxa <h2sxxa0w0@gmail.com>
2024-03-03 22:04:21 +0800

    ✨ Scrollable Adapt

--- lib/views/pages/notifications.dart
+++ lib/views/pages/notifications.dart
@@ -20,159 +20,161 @@ class _NotificationsPageState extends State<NotificationsPage> {
       appBar: AppBar(
         title: const Text("通知设置"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Card(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              top: 12,
-              bottom: 12,
-            ),
-            child: Wrap(
-              children: [
-                SwitchListTile(
-                  secondary: const Icon(Icons.notifications_on),
-                  title: const Text("启用通知"),
-                  subtitle: const Text("Enable Notifications"),
-                  value: configs.notificationsEnable.getOr(false),
-                  onChanged: (bool value) async {
-                    setState(() {
-                      configs.notificationsEnable.write(value);
-                    });
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 12,
+                bottom: 12,
+              ),
+              child: Wrap(
+                children: [
+                  SwitchListTile(
+                    secondary: const Icon(Icons.notifications_on),
+                    title: const Text("启用通知"),
+                    subtitle: const Text("Enable Notifications"),
+                    value: configs.notificationsEnable.getOr(false),
+                    onChanged: (bool value) async {
+                      setState(() {
+                        configs.notificationsEnable.write(value);
+                      });
 
-                    if (value) {
-                      await Scheduler.scheduleAll(context);
-                    } else {
-                      await Scheduler.cancelAll();
-                    }
-                  },
-                ),
-                SwitchListTile(
-                  secondary: const Icon(Icons.notifications_on),
-                  title: const Text("仅计划今日通知"),
-                  subtitle: const Text("Day Schedule"),
-                  value: configs.notificationsDay.getOr(false),
-                  onChanged: (bool value) async {
-                    setState(() {
-                      configs.notificationsDay.write(value);
-                    });
-
-                    Scheduler.reScheduleAll(context);
-                  },
-                ),
-                Visibility(
-                  visible: configs.notificationsEnable.getOr(false),
-                  child: ListTile(
-                    leading: const Icon(Icons.refresh),
-                    title: const Text("重新计划通知"),
-                    subtitle: const Text("reSchedule"),
-                    onTap: () {
-                      Scheduler.reScheduleAll(context);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text("重新计划完成")));
+                      if (value) {
+                        await Scheduler.scheduleAll(context);
+                      } else {
+                        await Scheduler.cancelAll();
+                      }
                     },
                   ),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.timer),
-                  title: const Text("课前提醒"),
-                  subtitle: const Text("Course Reminder"),
-                  trailing:
-                      Text("${configs.notificationsReminder.getOr(15)} 分钟"),
-                  onTap: () => ComplexDialog.instance
-                      .input(
-                          context: context,
-                          autofocus: true,
-                          title: const Text("输入整数"),
-                          decoration: const InputDecoration(
-                            labelText: "课前提醒(分钟)",
-                            border: OutlineInputBorder(),
-                          ),
-                          keyboardType: TextInputType.number)
-                      .then(
-                    (value) {
-                      if (value == null) {
-                        return;
-                      }
-                      var reminder = int.tryParse(value);
-
-                      if (reminder == null) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text("\"$value\" 不是一个整数")));
-                        return;
-                      }
-
+                  SwitchListTile(
+                    secondary: const Icon(Icons.notifications_on),
+                    title: const Text("仅计划今日通知"),
+                    subtitle: const Text("Day Schedule"),
+                    value: configs.notificationsDay.getOr(false),
+                    onChanged: (bool value) async {
                       setState(() {
-                        configs.notificationsReminder.write(reminder);
+                        configs.notificationsDay.write(value);
                       });
 
                       Scheduler.reScheduleAll(context);
                     },
                   ),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.schedule),
-                  title: const Text("查看计划中的通知"),
-                  subtitle: const Text("Notifications"),
-                  onTap: () => Scheduler.getScheduleNotifications().then(
-                    (value) => value.isNotEmpty
-                        ? showModalBottomSheet(
-                            context: context,
-                            builder: (context) {
-                              return Padding(
-                                padding: const EdgeInsets.all(8),
-                                child: ListView(
-                                  children: value
-                                      .map(
-                                        (e) => Card(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .outlineVariant,
-                                          child: ListTile(
-                                            title: Text(e.title.toString()),
-                                            subtitle: Text(e.body.toString()),
-                                          ),
-                                        ),
-                                      )
-                                      .toList(),
-                                ),
-                              );
-                            },
-                          )
-                        : ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text("暂无计划中的通知"))),
+                  Visibility(
+                    visible: configs.notificationsEnable.getOr(false),
+                    child: ListTile(
+                      leading: const Icon(Icons.refresh),
+                      title: const Text("重新计划通知"),
+                      subtitle: const Text("reSchedule"),
+                      onTap: () {
+                        Scheduler.reScheduleAll(context);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text("重新计划完成")));
+                      },
+                    ),
                   ),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.admin_panel_settings),
-                  title: const Text("通知权限"),
-                  subtitle: const Text("Notification Permission"),
-                  onTap: () {
-                    var plugin = Scheduler.plugin
-                        .resolvePlatformSpecificImplementation<
-                            AndroidFlutterLocalNotificationsPlugin>();
-                    plugin?.requestNotificationsPermission().then(
-                          (notification) =>
-                              plugin.requestExactAlarmsPermission().then(
-                                    (alarm) => ComplexDialog.instance.text(
-                                      context: context,
-                                      title: const Text("权限状态"),
-                                      content: Text(
-                                          "通知:$notification，精确通知:$alarm\n如果关闭应用无通知，请查询如何让你的手机系统允许应用后台行为"),
-                                    ),
+                  ListTile(
+                    leading: const Icon(Icons.timer),
+                    title: const Text("课前提醒"),
+                    subtitle: const Text("Course Reminder"),
+                    trailing:
+                        Text("${configs.notificationsReminder.getOr(15)} 分钟"),
+                    onTap: () => ComplexDialog.instance
+                        .input(
+                            context: context,
+                            autofocus: true,
+                            title: const Text("输入整数"),
+                            decoration: const InputDecoration(
+                              labelText: "课前提醒(分钟)",
+                              border: OutlineInputBorder(),
+                            ),
+                            keyboardType: TextInputType.number)
+                        .then(
+                      (value) {
+                        if (value == null) {
+                          return;
+                        }
+                        var reminder = int.tryParse(value);
+
+                        if (reminder == null) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text("\"$value\" 不是一个整数")));
+                          return;
+                        }
+
+                        setState(() {
+                          configs.notificationsReminder.write(reminder);
+                        });
+
+                        Scheduler.reScheduleAll(context);
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.schedule),
+                    title: const Text("查看计划中的通知"),
+                    subtitle: const Text("Notifications"),
+                    onTap: () => Scheduler.getScheduleNotifications().then(
+                      (value) => value.isNotEmpty
+                          ? showModalBottomSheet(
+                              context: context,
+                              builder: (context) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: ListView(
+                                    children: value
+                                        .map(
+                                          (e) => Card(
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .outlineVariant,
+                                            child: ListTile(
+                                              title: Text(e.title.toString()),
+                                              subtitle: Text(e.body.toString()),
+                                            ),
+                                          ),
+                                        )
+                                        .toList(),
                                   ),
-                        );
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(Icons.bug_report),
-                  title: const Text("测试通知"),
-                  subtitle: const Text("Test Notification"),
-                  onTap: () {
-                    Scheduler.scheduleTest();
-                  },
-                )
-              ],
+                                );
+                              },
+                            )
+                          : ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text("暂无计划中的通知"))),
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.admin_panel_settings),
+                    title: const Text("通知权限"),
+                    subtitle: const Text("Notification Permission"),
+                    onTap: () {
+                      var plugin = Scheduler.plugin
+                          .resolvePlatformSpecificImplementation<
+                              AndroidFlutterLocalNotificationsPlugin>();
+                      plugin?.requestNotificationsPermission().then(
+                            (notification) =>
+                                plugin.requestExactAlarmsPermission().then(
+                                      (alarm) => ComplexDialog.instance.text(
+                                        context: context,
+                                        title: const Text("权限状态"),
+                                        content: Text(
+                                            "通知:$notification，精确通知:$alarm\n如果关闭应用无通知，请查询如何让你的手机系统允许应用后台行为"),
+                                      ),
+                                    ),
+                          );
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.bug_report),
+                    title: const Text("测试通知"),
+                    subtitle: const Text("Test Notification"),
+                    onTap: () {
+                      Scheduler.scheduleTest();
+                    },
+                  )
+                ],
+              ),
             ),
           ),
         ),
