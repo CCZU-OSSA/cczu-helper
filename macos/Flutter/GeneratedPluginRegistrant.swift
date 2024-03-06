2e4ee404401f3439340b856d2d84f6d79ea85c85
H2Sxxa <h2sxxa0w0@gmail.com>
2024-03-06 10:04:39 +0800

    ✨ Add other Platform Suppot

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import dynamic_color
+import flutter_local_notifications
+import path_provider_foundation
+import share_plus
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
